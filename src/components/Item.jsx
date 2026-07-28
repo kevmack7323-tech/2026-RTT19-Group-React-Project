678a0a2c24b9f81b57ef7a0a5d3bba05593cc13a
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-07-28 11:24:30 -0400

    Improved movie item display

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,18 @@
 function Item({ movie }) {
-    return (
-        <div>
-            <h2>{movie.Title}</h2>
-            <img
-            src={movie.Poster}
-            alt={movie.Title}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <img 
+        src={movie.Poster} 
+        alt={movie.Title}
+      />
+
+      <h2>{movie.Title}</h2>
+
+      <p>Release Year: {movie.Year}</p>
+
+      <p>Type: {movie.Type}</p>
+    </div>
+  );
 }
+
+export default Item;
